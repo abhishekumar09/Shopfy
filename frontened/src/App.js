cc9c4e8d46a521af6493a5e957271e4ebb2a672b
abhishekumar09 <2003abhishek.09@gmail.com>
2024-06-15 18:20:45 +0530

    react app

---
+++ frontened/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-red-300 ">
+     kjnm
+    </div>
+  );
+}
+
+export default App;
---
+++ frontened/src/routes/index.js
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from 'react-router-dom'
+import App from '../App'
+
+
+const router = createBrowserRouter([
+
+{
+    path: "/",
+    element : <App/>
+}
+
+
+
+])
+
+export default router
